ccfabc9d23ade484cb63908ebdf9d27df115c474
glynternet <1295698+glynternet@users.noreply.github.com>
2023-10-21 17:25:34 -0600

    Brew formula update for retry version 0.9.0-rc1

--- Formula/retry.rb
+++ Formula/retry.rb
@@ -5,21 +5,21 @@
 class Retry < Formula
   desc "The command for retrying commands"
   homepage "https://github.com/glynternet/retry"
-  version "0.8.1"
+  version "0.9.0-rc1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/glynternet/retry/releases/download/0.8.1/retry_0.8.1_darwin_amd64.tar.gz"
-      sha256 "ff4e36f5e100e0b33aeb356cba8fe9c02eab52c5601ea926346c27f9e31d6094"
+      url "https://github.com/glynternet/retry/releases/download/0.9.0-rc1/retry_0.9.0-rc1_darwin_amd64.tar.gz"
+      sha256 "74e98c82c82f4022c7d597ba8b8f4cabd2f22a3f5e2d69b087f5a5b9a138d524"
 
       def install
         bin.install "retry"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/glynternet/retry/releases/download/0.8.1/retry_0.8.1_darwin_arm64.tar.gz"
-      sha256 "1c18b75e036bd69ff980923f4221f8fc5973646b1aa08f6b901deec0f8b7d026"
+      url "https://github.com/glynternet/retry/releases/download/0.9.0-rc1/retry_0.9.0-rc1_darwin_arm64.tar.gz"
+      sha256 "741c518ab2a757baf6557acbbf8ce6dd48fcd3bf11c04f6a776ca1336e5669d3"
 
       def install
         bin.install "retry"
@@ -29,16 +29,16 @@ class Retry < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/glynternet/retry/releases/download/0.8.1/retry_0.8.1_linux_arm64.tar.gz"
-      sha256 "147c2e3edb98f427001219086240e9cdfeac0a138ce367db27ea188e9043b602"
+      url "https://github.com/glynternet/retry/releases/download/0.9.0-rc1/retry_0.9.0-rc1_linux_arm64.tar.gz"
+      sha256 "61d9cac33b610ad86f66bcb3f57b5647052bf467f7d9c41ddbd64676bf58cca9"
 
       def install
         bin.install "retry"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/glynternet/retry/releases/download/0.8.1/retry_0.8.1_linux_amd64.tar.gz"
-      sha256 "bb1173b2e1eeadd0c743ac630e4bfa3ac89ce92b213dd89747ab0f4fc74b0d0f"
+      url "https://github.com/glynternet/retry/releases/download/0.9.0-rc1/retry_0.9.0-rc1_linux_amd64.tar.gz"
+      sha256 "06b76e19e9a7b8af4fda6a49dcb3439bb1b1ad3971d8582465d7a3b302dd62c4"
 
       def install
         bin.install "retry"
